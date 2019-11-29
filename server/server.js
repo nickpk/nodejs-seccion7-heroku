6b2354614c342cee51032f93f2b520624dd5ee08
Diego Pastoriza <dpastori@everis.com>
2019-11-29 15:24:24 -0300

    merge

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+require('./config/config');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/usuario', function (req, res) {
+  res.json('get usuario')
+});
+
+app.post('/usuario', function (req, res) {
+    let body = req.body;
+    res.json({
+        persona:body
+    })
+});
+
+app.put('/usuario/:id', function (req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    })
+});
+
+app.delete('/usuario', function (req, res) {
+    res.json('delete usuario')
+});
+ 
+app.listen(process.env.PORT, ()=> {
+    console.log(`escuchando en puerto ${process.env.PORT}`);
+})
