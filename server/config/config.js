a0d6d2159d30d63b7056d06fa4e54b6f119df49e
Diego Pastoriza <dpastori@everis.com>
2019-12-04 11:44:19 -0300

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -7,10 +7,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-/*if(process.env.NODE_ENV === 'dev'){
+if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb:http://localhost:27017/cafe';
-}else{*/
-    urlDB = 'mongodb+srv://admin:2gKOLd8IWOULLU5H@clouster-nodejs-curso-1nt4w.mongodb.net/cafe';
+}else{
+    urlDB = process.env.MONGO_URL;
 //}
 
 process.env.urlDB = urlDB;
